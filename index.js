ab563931947766663d1254aa238260bacbc6db23
Garrett Bennett <bennett.g00@gmail.com>
2024-03-10 22:54:38 -0600

    Add function to make sure words are valid

--- index.js
+++ index.js
@@ -16,3 +16,55 @@ app.use((_req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+// add words for testing
+const words = new Set();
+words.add('dad');
+words.add('dude');
+
+function checkBoard(board) {
+    if (board === null || board.length === 0 || board[0].length === 0) {
+        return false;
+    }
+
+    let word = '';
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] !== ' ') {
+                word += board[i][j];
+            } else if (word.length > 0) {
+                if (word.length > 1 && !words.has(word)) {
+                    return false;
+                }
+                word = '';
+            }
+        }
+        if (word.length > 0) {
+            if (word.length > 1 && !words.has(word)) {
+                return false;
+            }
+            word = '';
+        }
+    }
+
+    for (let j = 0; j < board[0].length; j++) {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][j] !== ' ') {
+                word += board[i][j];
+            } else if (word.length > 0) {
+                if (word.length > 1 && !words.has(word)) {
+                    return false;
+                }
+                word = '';
+            }
+        }
+        if (word.length > 0) {
+            if (word.length > 1 && !words.has(word)) {
+                return false;
+            }
+            word = '';
+        }
+    }
+
+    return true;
+}
