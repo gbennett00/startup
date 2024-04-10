a917c8fc8e9fbccb41a9316a36210c410b43279e
Garrett Bennett <bennett.g00@gmail.com>
2024-04-10 17:20:55 -0600

    Finish app component and stub view components

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className='body'>
+      <header class="bg-primary container-fluid">
+          <nav class="navbar fixed-top navbar-dark">
+            <a class="navbar-brand">WordBlitz<sup>&reg;</sup></a>
+            <menu class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="start.html">Start</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="game.html">Game</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="profile.html">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+              </li>
+            </menu>
+          </nav>
+        </header>
+      <main>App components go here</main>
+      <footer class="bg-secondary">
+          <div>
+            <span class="text-reset">Author: Garrett B.</span>
+            <a class="text-light px-3" href="https://github.com/gbennett00/startup">GitHub</a>
+          </div>
+        </footer>
+    </div>);
 }
---
+++ src/start/start.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Start() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>start displayed here</div>
+    </main>
+  );
+}
